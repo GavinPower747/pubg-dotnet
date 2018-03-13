901e8605d101daee34751182cff720683dbc36cd
ItsNovaHere <theawesome624@hotmail.com>
2018-03-13 17:37:31 -0500

    Region Enum

---
+++ src/pubg-dotnet/Infrastructure/Region.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Pubg.Net.Infrastructure
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum Region
+    {
+         //Xbox Regions
+         [EnumMember(Value = "xbox-as")]
+         XboxAsia,
+
+         [EnumMember(Value = "xbox-eu")]
+         XboxEurope,
+
+         [EnumMember(Value = "xbox-na")]
+         XboxNorthAmerica,
+
+         [Region("xbox-oc")]
+         XboxOceania
+    }
+}
--- src/pubg-dotnet/Services/Matches/PubgMatchService.cs
+++ src/pubg-dotnet/Services/Matches/PubgMatchService.cs
@@ -1,4 +1,5 @@
-﻿using Pubg.Net.Infrastructure;
+﻿using Newtonsoft.Json;
+using Pubg.Net.Infrastructure;
 using Pubg.Net.Values;
 using System.Collections.Generic;
 using System.Threading;
@@ -8,39 +9,39 @@ namespace Pubg.Net
 {
     public class PubgMatchService : PubgService
     {
-        public PubgMatchService() : base(null) { }
+        public PubgMatchService() : base() { }
         public PubgMatchService(string apiKey) : base(apiKey) { }
 
-        public virtual PubgMatch GetMatch(string regionId, string matchId)
+        public virtual PubgMatch GetMatch(Region region, string matchId)
         {
-            var url = string.Format(Api.Matches.MatchesEndpoint + "/{1}", regionId, matchId);
+            var url = string.Format(Api.Matches.MatchesEndpoint + "/{1}", JsonConvert.ToString(region), matchId);
 
             var matchJson = HttpRequestor.GetString(url, ApiKey);
 
             return JsonMapper<PubgMatch>.MapObject(matchJson, ResponseRootNode);
         }
 
-        public async virtual Task<PubgMatch> GetMatchAsync(string regionId, string matchId, CancellationToken cancellationToken = default(CancellationToken))
+        public async virtual Task<PubgMatch> GetMatchAsync(Region region, string matchId, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var url = string.Format(Api.Matches.MatchesEndpoint + "/{1}", regionId, matchId);
+            var url = string.Format(Api.Matches.MatchesEndpoint + "/{1}", JsonConvert.ToString(region), matchId);
 
             var matchJson = await HttpRequestor.GetStringAsync(url, ApiKey, cancellationToken);
 
             return JsonMapper<PubgMatch>.MapObject(matchJson, ResponseRootNode);
         }
 
-        public virtual IEnumerable<PubgMatch> GetMatches(string regionId, GetPubgMatchRequest request)
+        public virtual IEnumerable<PubgMatch> GetMatches(Region region, GetPubgMatchRequest request)
         {
-            var url = string.Format(Api.Matches.MatchesEndpoint, regionId);
+            var url = string.Format(Api.Matches.MatchesEndpoint, JsonConvert.ToString(region));
 
             var collectionJson = HttpRequestor.GetString(url, ApiKey);
 
             return JsonMapper<PubgMatch>.MapCollection(collectionJson, ResponseRootNode);
         }
 
-        public async virtual Task<IEnumerable<PubgMatch>> GetMatchesAsync(string regionId, GetPubgMatchRequest request, CancellationToken cancellationToken = default(CancellationToken))
+        public async virtual Task<IEnumerable<PubgMatch>> GetMatchesAsync(Region region, GetPubgMatchRequest request, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var url = string.Format(Api.Matches.MatchesEndpoint, regionId);
+            var url = string.Format(Api.Matches.MatchesEndpoint, JsonConvert.ToString(region));
 
             var collectionJson = await HttpRequestor.GetStringAsync(url, ApiKey, cancellationToken);
 
