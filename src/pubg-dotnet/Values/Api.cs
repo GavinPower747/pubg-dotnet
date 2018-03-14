4e95463fe88c3e714e3cd7b699d2ee9dac89f6eb
Gavin Power <gavin@transportzone.com>
2018-03-14 17:50:25 +0000

    Added sharded uri as not all endpoints are sharded

--- src/pubg-dotnet/Values/Api.cs
+++ src/pubg-dotnet/Values/Api.cs
@@ -3,10 +3,11 @@
     internal static class Api
     {
         internal const string DefaultBaseUrl = "https://api.playbattlegrounds.com/shards/{0}";
+        internal static string ShardedBaseUrl = PubgApiConfiguration.GetApiBase() + "/shards/{0}";
 
         internal static class Matches
         {
-            internal static string MatchesEndpoint = PubgApiConfiguration.GetApiBase() + "/matches";
+            internal static string MatchesEndpoint = ShardedBaseUrl + "/matches";
         }
 
         internal static class Status
