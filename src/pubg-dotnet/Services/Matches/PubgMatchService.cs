1cca7be62f648f2c8539a15561434f09ebab2731
Gavin Power <gavin@transportzone.com>
2018-03-14 17:38:06 +0000

    Updated match service with new Url generation

--- src/pubg-dotnet/Services/Matches/PubgMatchService.cs
+++ src/pubg-dotnet/Services/Matches/PubgMatchService.cs
@@ -1,4 +1,5 @@
-﻿using Pubg.Net.Infrastructure;
+﻿using Pubg.Net.Services;
+using Pubg.Net.Infrastructure;
 using Pubg.Net.Values;
 using System.Collections.Generic;
 using System.Threading;
@@ -11,36 +12,40 @@ namespace Pubg.Net
         public PubgMatchService() : base(null) { }
         public PubgMatchService(string apiKey) : base(apiKey) { }
 
-        public virtual PubgMatch GetMatch(string regionId, string matchId)
+        public virtual PubgMatch GetMatch(string regionId, string matchId, string apiKey = null)
         {
             var url = string.Format(Api.Matches.MatchesEndpoint + "/{1}", regionId, matchId);
+            apiKey = string.IsNullOrEmpty(apiKey) ? ApiKey : apiKey;
 
-            var matchJson = HttpRequestor.GetString(url, ApiKey);
+            var matchJson = HttpRequestor.GetString(url, apiKey);
 
             return JsonMapper<PubgMatch>.MapObject(matchJson, ResponseRootNode);
         }
 
-        public async virtual Task<PubgMatch> GetMatchAsync(string regionId, string matchId, CancellationToken cancellationToken = default(CancellationToken))
+        public async virtual Task<PubgMatch> GetMatchAsync(string regionId, string matchId, string apiKey = null, CancellationToken cancellationToken = default(CancellationToken))
         {
             var url = string.Format(Api.Matches.MatchesEndpoint + "/{1}", regionId, matchId);
+            apiKey = string.IsNullOrEmpty(apiKey) ? ApiKey : apiKey;
 
-            var matchJson = await HttpRequestor.GetStringAsync(url, ApiKey, cancellationToken);
+            var matchJson = await HttpRequestor.GetStringAsync(url, apiKey, cancellationToken);
 
             return JsonMapper<PubgMatch>.MapObject(matchJson, ResponseRootNode);
         }
 
         public virtual IEnumerable<PubgMatch> GetMatches(string regionId, GetPubgMatchRequest request)
         {
-            var url = string.Format(Api.Matches.MatchesEndpoint, regionId);
+            var url = RequestBuilder.BuildRequestUrl(string.Format(Api.Matches.MatchesEndpoint, regionId), request);
+            var apiKey = string.IsNullOrEmpty(request.ApiKey) ? ApiKey : request.ApiKey;
 
-            var collectionJson = HttpRequestor.GetString(url, ApiKey);
+            var collectionJson = HttpRequestor.GetString(url, apiKey);
 
             return JsonMapper<PubgMatch>.MapCollection(collectionJson, ResponseRootNode);
         }
 
         public async virtual Task<IEnumerable<PubgMatch>> GetMatchesAsync(string regionId, GetPubgMatchRequest request, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var url = string.Format(Api.Matches.MatchesEndpoint, regionId);
+            var url = RequestBuilder.BuildRequestUrl(string.Format(Api.Matches.MatchesEndpoint, regionId), request);
+            var apiKey = string.IsNullOrEmpty(request.ApiKey) ? ApiKey : request.ApiKey;
 
             var collectionJson = await HttpRequestor.GetStringAsync(url, ApiKey, cancellationToken);
 
