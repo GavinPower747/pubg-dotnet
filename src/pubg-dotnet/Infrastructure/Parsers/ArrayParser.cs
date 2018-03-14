0624a03f948c19cd0af940e5b5b9580d39e96b82
Gavin Power <gavin@transportzone.com>
2018-03-14 17:35:11 +0000

    Added code to parse through request objects and build the query

---
+++ src/pubg-dotnet/Infrastructure/Parsers/ArrayParser.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Reflection;
+
+namespace Pubg.Net.Infrastructure.Parsers
+{
+    internal class ArrayParser : IParser
+    {
+        public bool CanParse(PropertyInfo prop, object propValue) => prop.PropertyType.IsArray;
+
+        public string Parse(PropertyInfo prop, object propValue) => ((IEnumerable)propValue).Cast<object>().Select(x => x.ToString()).Aggregate((x,y) => x + "," + y);
+    }
+}
