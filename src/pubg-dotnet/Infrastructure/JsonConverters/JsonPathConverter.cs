d7081b41fa8b37401dcfc445a43facc7fb72746a
Gavin Power <gavin@transportzone.com>
2018-03-16 01:07:57 +0000

    Json converter for specific paths

---
+++ src/pubg-dotnet/Infrastructure/JsonConverters/JsonPathConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Pubg.Net.Infrastructure.JsonConverters
+{
+    public class JsonPathConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType) => false;
+        public override bool CanWrite => false;
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jo = JObject.Load(reader);
+            object targetObj = Activator.CreateInstance(objectType);
+
+            foreach (PropertyInfo prop in objectType.GetProperties().Where(p => p.CanRead && p.CanWrite))
+            {
+                var attribute = prop.GetCustomAttribute<JsonPropertyAttribute>();
+
+                string jsonPath = (attribute != null ? attribute.PropertyName : prop.Name);
+                JToken token = jo.SelectToken(jsonPath);
+
+                if (token != null && token.Type != JTokenType.Null)
+                {
+                    object value = token.ToObject(prop.PropertyType, serializer);
+                    prop.SetValue(targetObj, value, null);
+                }
+            }
+
+            return targetObj;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) { }
+    }
+}
