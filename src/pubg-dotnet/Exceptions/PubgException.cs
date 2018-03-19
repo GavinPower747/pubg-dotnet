549cd945cc21bfbdb3ef21c63a6e409858174ae0
Gavin Power <gavin@transportzone.com>
2018-03-19 16:58:24 +0000

    Changed error handling to fit JSON API spec

--- src/pubg-dotnet/Exceptions/PubgException.cs
+++ src/pubg-dotnet/Exceptions/PubgException.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Pubg.Net.Models.Errors;
+using System;
+using System.Collections.Generic;
 using System.Net;
 
 namespace Pubg.Net.Exceptions
@@ -6,9 +8,15 @@ namespace Pubg.Net.Exceptions
     public class PubgException : Exception
     {
         public HttpStatusCode HttpStatusCode { get; set; }
+        public IEnumerable<PubgError> Errors { get; set; }
 
         public PubgException(string errorMessage) : base(errorMessage) { }
         public PubgException(string errorMessage, Exception innerException) : base(errorMessage, innerException) { }
         public PubgException(string errorMessage, HttpStatusCode statusCode) : base(errorMessage) { }
+        public PubgException(string errorMessage, HttpStatusCode statusCode, IEnumerable<PubgError> apiErrors) : base(errorMessage)
+        {
+            HttpStatusCode = statusCode;
+            Errors = apiErrors;
+        }
     }
 }
---
+++ src/pubg-dotnet/Models/Errors/PubgError.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Pubg.Net.Models.Base;
+
+namespace Pubg.Net.Models.Errors
+{
+    public class PubgError : PubgEntity
+    {
+        [JsonProperty]
+        public string Status { get; set; }
+
+        [JsonProperty]
+        public string Code { get; set; }
+        
+        [JsonProperty]
+        public string Title { get; set; }
+
+        [JsonProperty]
+        public string Detail { get; set; }
+
+        [JsonProperty]
+        public PubgErrorSource Source { get; set; }
+    }
+}
