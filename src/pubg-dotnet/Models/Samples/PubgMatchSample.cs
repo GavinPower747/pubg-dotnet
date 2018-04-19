bb68a75e768b513870d3b5081626c67965487459
Gavin Power <gavin@transportzone.com>
2018-04-19 16:16:57 +0100

    Added samples service and tests

---
+++ src/pubg-dotnet/Models/Samples/PubgMatchSample.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Pubg.Net.Infrastructure.JsonConverters;
+using Pubg.Net.Models.Base;
+using System;
+using System.Collections.Generic;
+
+namespace Pubg.Net.Models.Samples
+{
+    public class PubgMatchSample : PubgShardedEntity
+    {
+        [JsonProperty]
+        public DateTime CreatedAt { get; set; }
+        
+        [JsonProperty]
+        public string TitleId { get; set; }
+
+        [JsonProperty("matches")]
+        [JsonConverter(typeof(RelationshipIdConverter))]
+        public IEnumerable<string> MatchIds { get; set; }
+    }
+}
