2af7758e8eaeac2c942521ad9578da433f59fb28
Gavin Power <gavin@transportzone.com>
2018-04-30 13:24:33 +0100

    Added death type enum

---
+++ src/pubg-dotnet/Models/Participants/PubgParticipantDeathType.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace Pubg.Net.Models.Participants
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum PubgParticipantDeathType
+    {
+        [EnumMember(Value = "alive")]
+        Alive,
+        [EnumMember(Value = "byplayer")]
+        ByPlayer,
+        [EnumMember(Value = "suicide")]
+        Suicide
+    }
+}
