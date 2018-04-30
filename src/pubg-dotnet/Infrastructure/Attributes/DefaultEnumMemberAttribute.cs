dd3e35ce16b41a2cbd65766165ca1ef9e63f0b90
Gavin Power <gavin@transportzone.com>
2018-04-30 14:13:11 +0100

    Added default values for enum string conversion

---
+++ src/pubg-dotnet/Infrastructure/Attributes/DefaultEnumMemberAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Pubg.Net.Infrastructure.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public sealed class DefaultEnumMemberAttribute : Attribute
+    {
+    }
+}
