b9ee2221fa0c66df46c35ce75fabc2499e33ec29
Gavin Power <gavin@transportzone.com>
2018-06-22 14:58:26 +0100

    Added new Telemetry Events

---
+++ src/pubg-dotnet/Models/Telemetry/Events/LogPlayerMakeGroggy.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace Pubg.Net.Models.Telemetry.Events
+{
+    public class LogPlayerMakeGroggy : PubgTelemetryEvent
+    {
+        [JsonProperty]
+        public int AttackerId { get; set; }
+
+        [JsonProperty]
+        public PubgCharacter Attacker { get; set; }
+
+        [JsonProperty]
+        public PubgCharacter Victim { get; set; }
+
+        [JsonProperty]
+        public string DamageTypeCategory { get; set; }
+
+        [JsonProperty]
+        public string DamageCauserName { get; set; }
+
+        [JsonProperty]
+        public float Distance { get; set; }
+
+        [JsonProperty]
+        public bool IsAttackerInVehicle { get; set; }
+
+        [JsonProperty]
+        public int DBNOId { get; set; }
+    }
+}
