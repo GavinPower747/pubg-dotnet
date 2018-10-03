22db8432f38164cf9a52ce31f354f7a94eda0af5
Gavin Power <gavin@transportzone.com>
2018-10-03 11:51:14 +0100

    Updated tests and test infrastructure

--- test/pubg-dotnet.Tests/Seasons/SeasonsTests.cs
+++ test/pubg-dotnet.Tests/Seasons/SeasonsTests.cs
@@ -8,11 +8,22 @@ namespace pubg.net.Tests.Seasons
     public class SeasonsTests
     {
         [Fact]
-        public void Can_Get_Seasons()
+        public void Can_Get_Seasons_OnPC()
         {
             var seasonsService = new PubgSeasonService(Storage.ApiKey);
 
-            var seasons = seasonsService.GetSeasons(PubgRegion.PCEurope);
+            var seasons = seasonsService.GetSeasonsPC();
+
+            Assert.All(seasons, s => s.Id.Should().NotBeNull());
+            seasons.Should().ContainSingle(s => s.IsCurrentSeason == true);
+        }
+
+        [Fact]
+        public void Can_Get_Seasons_OnXbox()
+        {
+            var seasonsService = new PubgSeasonService(Storage.ApiKey);
+
+            var seasons = seasonsService.GetSeasonsXbox(PubgRegion.PCEurope);
 
             Assert.All(seasons, s => s.Id.Should().NotBeNull());
             seasons.Should().ContainSingle(s => s.IsCurrentSeason == true);
