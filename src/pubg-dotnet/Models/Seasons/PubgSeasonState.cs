59183f2443415afa06ae2dea0b5b3270b09845aa
Gavin Power <gavin@transportzone.com>
2018-12-12 01:05:39 +0000

    Added default value to Season State

--- src/pubg-dotnet/Models/Seasons/PubgSeasonState.cs
+++ src/pubg-dotnet/Models/Seasons/PubgSeasonState.cs
@@ -1,12 +1,15 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
+using Pubg.Net.Infrastructure.Attributes;
+using Pubg.Net.Infrastructure.JsonConverters;
 using System.Runtime.Serialization;
 
 namespace Pubg.Net.Models.Seasons
 {
-    [JsonConverter(typeof(StringEnumConverter))]
+    [JsonConverter(typeof(DefaultValueStringEnumConverter))]
     public enum PubgSeasonState
     {
+        [DefaultEnumMember]
+        Unknown,
         [EnumMember(Value = "closed")]
         Closed,
         [EnumMember(Value = "prepare")]
