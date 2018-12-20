e6e4b2d09ed167aeee68494d9a57701ae9082149
Gavin Power <gavinpower747@hotmail.com>
2018-12-20 11:58:15 +0000

    Updated DefaultEnumConverter
    Changed to use default value on error

--- src/pubg-dotnet/Infrastructure/JsonConverters/DefaultValueStringEnumConverter.cs
+++ src/pubg-dotnet/Infrastructure/JsonConverters/DefaultValueStringEnumConverter.cs
@@ -10,14 +10,19 @@ namespace Pubg.Net.Infrastructure.JsonConverters
     {
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            if(string.IsNullOrEmpty(reader.Value.ToString()))
+            Enum convertedObject;
+            
+            try
+            {
+                convertedObject = base.ReadJson(reader, objectType, existingValue, serializer);
+            }
+            catch(Exception ex)
             {
                 var defaultMember = objectType.GetMembers().FirstOrDefault(f => f.GetCustomAttributes(typeof(DefaultEnumMemberAttribute), false).Count() > 0);
-
-                return Enum.Parse(objectType, defaultMember.Name);
+                convertedObject = Enum.Parse(objectType, defaultMember.Name);
             }
 
-            return base.ReadJson(reader, objectType, existingValue, serializer);
+            return convertedObject;
         }
     }
 }
