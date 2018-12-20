fb5d12009c20e46faa98f2c680fa6bdf0bf6de97
Gavin Power <gavinpower747@hotmail.com>
2018-12-20 12:13:21 +0000

    Added Tests for DefaultValueStringEnumConverter

---
+++ test/pubg-dotnet.UnitTests/JsonConverters/DefaultEnumConverterTests.cs
@@ -0,0 +1,71 @@
+using FluentAssertions;
+using JsonApiSerializer;
+using Newtonsoft.Json;
+using Pubg.Net.Infrastructure.JsonConverters;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Pubg.Net.UnitTests.JsonConverters
+{
+    public class RelationshipIdConverterTests
+    {
+        public class TestArticle
+        {
+            [JsonProperty]
+            public int Id { get; set; }
+
+            [JsonProperty]
+            public TestEnum TestEnum { get; set; }
+        }
+        
+        [JsonConverter(typeof(DefaultValueStringEnumConverter))]
+        public enum TestEnum
+        {
+          [DefaultEnumMember]
+          Default = 0,
+          NonDefault = 1
+        }
+
+        string jsonApiLiteral =
+            @"{
+                ""data"": 
+                {
+                  ""type"": ""TestArticle"",
+                  ""id"": ""1"",
+                  ""attributes"": 
+                  {
+                    ""testEnum"": ""%%ENUM_VALUE%%"",
+                  }
+                }
+            }";
+
+        [Fact]
+        public void DefaultValueStringEnumConverter_Can_ConvertToValue()
+        {
+            var value = TestEnum.NonDefault;
+            var valueJson = jsonApiLiteral.Replace("%%ENUM_VALUE%%", value.ToString())
+            var obj = JsonConvert.DeserializeObject<TestArticle>(valueJson);
+            
+            obj.TestEnum.Should().Equals(value);
+        }
+        
+        [Fact]
+        public void DefaultValueStringEnumConverter_Can_ConvertFromEmptyString()
+        {
+            var valueJson = jsonApiLiteral.Replace("%%ENUM_VALUE%%", "")
+            var obj = JsonConvert.DeserializeObject<TestArticle>(valueJson);
+            
+            obj.TestEnum.Should().Equals(value);
+        }
+        
+        [Fact]
+        DefaultValueStringEnumConverter_Can_ConvertFromUnknownValue()
+        {
+            var valueJson = jsonApiLiteral.Replace("%%ENUM_VALUE%%", "RandomValue")
+            var obj = JsonConvert.DeserializeObject<TestArticle>(valueJson);
+            
+            obj.TestEnum.Should().Equals(value);  
+        }
+    }
+}
