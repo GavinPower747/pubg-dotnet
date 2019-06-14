50543de0bdcdb1df50e24ce42156f009c114499a
Svein Løvmoen Ellingsen <sle@gamefraym.com>
2019-06-14 17:22:50 +0200

    Added missing game modes. Upped version to 2.2.1.

--- src/pubg-dotnet/Models/Match/PubgGameMode.cs
+++ src/pubg-dotnet/Models/Match/PubgGameMode.cs
@@ -33,6 +33,58 @@ namespace Pubg.Net
         [EnumMember(Value = "normal-squad")]
         NormalSquad,
         [EnumMember(Value = "normal-squad-fpp")]
-        NormalSquadFPP
+        NormalSquadFPP,
+
+        [EnumMember(Value = "conquest-duo")]
+        ConquestDuo,
+        [EnumMember(Value = "conquest-duo-fpp")]
+        ConquestDuoFPP,
+        [EnumMember(Value = "conquest-solo")]
+        ConquestSolo,
+        [EnumMember(Value = "conquest-solo-fpp")]
+        ConquestSoloFPP,
+        [EnumMember(Value = "conquest-squad")]
+        ConquestSquad,
+        [EnumMember(Value = "conquest-squad-fpp")]
+        ConquestSquadFPP,
+
+        [EnumMember(Value = "esports-duo")]
+        EsportsDuo,
+        [EnumMember(Value = "esports-duo-fpp")]
+        EsportsDuoFPP,
+        [EnumMember(Value = "esports-solo")]
+        EsportsSolo,
+        [EnumMember(Value = "esports-solo-fpp")]
+        EsportsSoloFPP,
+        [EnumMember(Value = "esports-squad")]
+        EsportsSquad,
+        [EnumMember(Value = "esports-squad-fpp")]
+        EsportsSquadFPP,
+
+        [EnumMember(Value = "war-duo")]
+        WarDuo,
+        [EnumMember(Value = "war-duo-fpp")]
+        WarDuoFPP,
+        [EnumMember(Value = "war-solo")]
+        WarSolo,
+        [EnumMember(Value = "war-solo-fpp")]
+        WarSoloFPP,
+        [EnumMember(Value = "war-squad")]
+        WarSquad,
+        [EnumMember(Value = "war-squad-fpp")]
+        WarSquadFPP,
+
+        [EnumMember(Value = "zombie-duo")]
+        ZombieDuo,
+        [EnumMember(Value = "zombie-duo-fpp")]
+        ZombieDuoFPP,
+        [EnumMember(Value = "zombie-solo")]
+        ZombieSolo,
+        [EnumMember(Value = "zombie-solo-fpp")]
+        ZombieSoloFPP,
+        [EnumMember(Value = "zombie-squad")]
+        ZombieSquad,
+        [EnumMember(Value = "zombie-squad-fpp")]
+        ZombieSquadFPP
     }
 }
